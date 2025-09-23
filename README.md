3758b3829d29e19fb9470047c53f8c35bd446b52
Shea Duma <nu@she-a.eu>
2025-09-23 17:46:30 +0300

    showing listing in a WM-like style

--- README.md
+++ README.md
@@ -2,3 +2,10 @@ WÃÃÃÃÃÃÃÃÃÃÃÃÃÃÃÃÃÃÃW
 
 **W**indow f**ĩ**le **W**atcher
 A react project that shows the user file structure through an api blob ran locally on the system at the port :33333
+
+The API outputs the given directory's files in the following form
+
+* ##--#--#-- 12.20G file1
+* -##-##-##- 4.000K file2
+
+Where the first part is \[directory]\[read]\[write]\[execute]\[read]\[write]\[execute]\[read]\[write]\[execute], in the order that `ls -l` would display the permissions and the second part is the size of the file so that the number has 4 digits and size type
--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,98 @@
 import { useState } from 'react'
+import { useEffect } from "react";
 import LeftLogo from '/house.svg'
-import './App.css'
+//import './App.css'
+import './format.css'
+import './other.css'
 import * as Cookies from 'es-cookie'
 
 async function filList() {
   const curPath = window.location.pathname
-//  console.log(curPath)
-  console.log(Cookies.get('oldPath'));
+//  console.log(Cookies.get('oldPath'));
 
   try {
     const res = await fetch('http://localhost/api/wawAPI/' + curPath)
-    const text = await res.text()
-//	console.log(text);
+    const text = await res.text();
+//	console.log(text.split('\n'));
 	Cookies.set('oldPath', curPath);
-    return text
+	if((curPath.match(/\//g) || []).length == 2 && curPath.substring(1, curPath.lastIndexOf('/')) == "home")
+		Cookies.set('Username', curPath.substring(curPath.lastIndexOf('/')+1));
+    return text.split('\n');
   } catch (err) {
     console.error('fetch failed:', err)
     return 'error fetching'
   }
 }
+function File({item}){
+	const path = item.substring(item.lastIndexOf(" "));
+	const name = item.substring(item.lastIndexOf("/")+1);
+	if(path.length > 0)
+		return (
+			<>
+			<a href={path} className="card">
+				<img src={LeftLogo} className="logo" />
+				<p> {name} </p>
+			</a>
+			</>
+		);
+	return;
+}
+function HomeButton(){
 
-function App() {
-	const [oldPath, setoldPath] = useState((!Cookies.get('oldPath'))? "NOT SET YET" : Cookies.get('oldPath'));
-	const [path, setPath] = useState(null);
 	return (
 		<>
-		<div>
-			<a href="/" target="_blank">
+			<a href="/home" className="nav">
 				<img src={LeftLogo} className="logo" />
 			</a>
+		</>
+	);
+}
+
+
+function NavBar(){
+	return (
+		<>
+			<div id="navbar">
+				<HomeButton />
+				<HomeButton />
+				<HomeButton />
+				<HomeButton />
+				<HomeButton />
+				<HomeButton />
+			</div>
+		</>
+	);
+}
+function App() {
+	const [oldPath, setoldPath] = useState((!Cookies.get('oldPath'))? "NOT SET YET" : Cookies.get('oldPath'));
+	const [path, setPath] = useState([]);
+	useEffect(() => {
+		filList().then(data => setPath(data));
+	}, []);
+	return (
+		<>
+		<div id="Items">
+		<div id="Left">
+			<NavBar />
 		</div>
-		<h1>The Team</h1>
-		<div className="card">
-			<p> THE OLD PATH IS {oldPath} </p>
-			<button onClick={() => {setPath(filList())}}>
-				GET THE PATH DUMMIE
-			</button>
+		<div id="Middle">
+			<div id="Upper">
+			<h1>The Team</h1>
+			</div>
+			<div id="Lower">
+			<div className="Files">
+			  {path.map((line, i) => (
+				<div key={i}>
+					<File item = {line} />
+				  </div>
+			  ))}
+			</div>
+			</div>
 		</div>
-		<p className="we" > {path} </p>
+		<div id="Right">
+			<p> Right </p>
+		</div>
+		</div>
 	</>
 	)
 }
